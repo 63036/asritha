a772b8a1f8a11e96f7c7a0641bd119f5e4dac78a
63036 <48045655+63036@users.noreply.github.com>
2019-03-06 12:27:57 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,6 @@
+import maximum
+c=maximum.add(5,8)
+print(c)
+from maximum import multiply
+c=multiply(5,8)
+print(c)
---
+++ sri.py
@@ -0,0 +1,32 @@
+def func1():	
+	print("hii")
+	print("hello")
+func1()
+
+def func2(a):
+	print("hii"+a)
+func2("asritha")
+
+def func3(university="IITB"):
+	print("i am in"+university)
+func3("IITC")
+func3("IIAT")
+func3()
+
+def func4(a,b):
+	print("hii another function")
+	print("c")
+	c=a+b
+	return c
+def func5():
+	print("hi")
+	d=func4(1,5)
+	print(d)
+func5()
+
+def func6(b,c,d):
+	e=b+c+d
+	print("b,c,d,e")
+func6(2,3,4)
+	
+
